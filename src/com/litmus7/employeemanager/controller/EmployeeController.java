c5d7030065769c1ba23e15fd0730beab0d0eacec
Rithas K <rithask@icloud.com>
2025-07-29 16:28:21 +0530

    add user input error handling

--- src/com/litmus7/employeemanager/controller/EmployeeController.java
+++ src/com/litmus7/employeemanager/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.litmus7.employeemanager.util.*;
 import java.io.File;
 import java.io.PrintWriter;
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,9 +15,11 @@ public class EmployeeController {
     public static void processTextFile() {
         String[] fileContent = TextFileUtil.readFromTextFile("employees.txt");
         List<Employee> employees = new ArrayList<>();
+
         for (String s : fileContent) {
             employees.add(TextFileUtil.sanitizeDataFromTextFile(s));
         }
+
         for (Employee emp : employees) {
             TextFileUtil.printEmployeeData(emp);
             System.out.println();
@@ -26,6 +29,7 @@ public class EmployeeController {
     public static void processCSVFile() {
         String[] fileContent = TextFileUtil.readFromTextFile("employees.txt");
         List<Employee> employees = new ArrayList<>();
+
         for (String s : fileContent) {
             Employee emp = TextFileUtil.sanitizeDataFromTextFile(s);
 
@@ -39,27 +43,68 @@ public class EmployeeController {
 
     public static void processUserInput() {
         Scanner sc = new Scanner(System.in);
-        Employee emp = new Employee();
 
-        System.out.print("Enter the ID: ");
-        emp.setId(Integer.parseInt(sc.nextLine()));
-        System.out.print("Enter the first name: ");
-        emp.setFirstName(sc.nextLine());
-        System.out.print("Enter the last name: ");
-        emp.setLastName(sc.nextLine());
-        System.out.print("Enter the mobile number: ");
-        emp.setMobileNo(Long.parseLong(sc.nextLine()));
-        System.out.print("Enter the email: ");
-        emp.setEmail(sc.nextLine());
-        System.out.print("Enter the joining date (YYYY-MM-DD): ");
-        emp.setJoiningDate(LocalDate.parse(sc.nextLine()));
+        int id = 0;
+        do {
+            System.out.print("Enter the ID: ");
+            try {
+                id = Integer.parseInt(sc.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("ID should be a number");
+                continue;
+            }
+        } while (!ValidationUtil.validateId(id));
+
+        String firstName;
+        do {
+            System.out.print("Enter the first name: ");
+            firstName = sc.nextLine();
+        } while (!ValidationUtil.validateName(firstName));
+
+        String lastName;
+        do {
+            System.out.print("Enter the last name: ");
+            lastName = sc.nextLine();
+        } while (!ValidationUtil.validateName(lastName));
+
+        Long mobileNo = 0L;
+        do {
+            System.out.print("Enter the 10-digit mobile number: ");
+            try {
+                mobileNo = Long.parseLong(sc.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Mobile number should only consist of digits");
+                continue;
+            }
+        } while (!ValidationUtil.validateMobileNo(mobileNo));
+
+        String email;
+        do {
+            System.out.print("Enter the email: ");
+            email = sc.nextLine();
+        } while (!ValidationUtil.validateEmail(email));
+
+        LocalDate joiningDate = null;
+        do {
+            System.out.print("Enter the joining date (YYYY-MM-DD): ");
+            try {
+                joiningDate = LocalDate.parse(sc.nextLine());
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid date");
+                continue;
+            }
+        } while (!ValidationUtil.validateJoiningDate(joiningDate));
+
         System.out.print("Is the employee active (true/false): ");
-        emp.setActive(Boolean.parseBoolean(sc.nextLine()));
+        boolean activeStatus = Boolean.parseBoolean(sc.nextLine());
 
+        Employee emp = new Employee(id, firstName, lastName, mobileNo, email, joiningDate, activeStatus);
         if (TextFileUtil.saveToCSV(emp)) {
             System.out.println("Added to CSV file successfully");
         } else {
             System.out.println("Adding failed");
         }
+
+        sc.close();
     }
 }
