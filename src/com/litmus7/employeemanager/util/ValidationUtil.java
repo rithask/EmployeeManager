a597797c05d33d5813069345138cb5336e7027e4
Rithas K <rithask@icloud.com>
2025-07-29 16:28:51 +0530

    change methods to static

--- src/com/litmus7/employeemanager/util/ValidationUtil.java
+++ src/com/litmus7/employeemanager/util/ValidationUtil.java
@@ -5,24 +5,24 @@ import java.time.LocalDate;
 public class ValidationUtil {
 
     // TODO: unique id check
-    public boolean validateId(int id) {
+    public static boolean validateId(int id) {
         return id > 0;
     }
 
-    public boolean validateName(String name) {
+    public static boolean validateName(String name) {
         return !name.isBlank();
     }
 
     // TODO: basic format check
-    public boolean validateEmail(String email) {
+    public static boolean validateEmail(String email) {
         return !email.isBlank();
     }
 
-    public boolean validateMobileNo(int number) {
+    public static boolean validateMobileNo(Long number) {
         return String.valueOf(number).length() == 10 && number > 0;
     }
 
-    public boolean validateJoiningDate(LocalDate date) {
-        return date.isAfter(LocalDate.parse("2009-06-30")) && date.isBefore(LocalDate.now());
+    public static boolean validateJoiningDate(LocalDate date) {
+        return date != null && date.isAfter(LocalDate.parse("2009-06-30")) && date.isBefore(LocalDate.now());
     }
 }
